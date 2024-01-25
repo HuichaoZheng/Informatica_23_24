f20a3922b2952f827d6084d0678c1e8b998176d3
HuichaoZheng <zheng.huichao@iisviolamarchesini.edu.it>
2024-01-25 21:58:09 +0100

    Css

--- ecommerce-main/views/admin/products/update.php
+++ ecommerce-main/views/admin/products/update.php
@@ -1,1 +1,50 @@
 <?php
+session_save_path("../../../sessions");
+session_start(); // Avvia la sessione
+
+if(!isset($_SESSION['current_user']))
+{
+    header('Location: ../../login.php');
+}
+    $id = $_POST['id'];
+    $nome = $_POST['nome'];
+    $prezzo = $_POST['prezzo'];
+    $marca = $_POST['marca'];
+?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modulo Informazioni Prodotto</title>
+</head>
+<body>
+<link rel="stylesheet" href="../../../css/styles.css">
+<div class="title">Crea</div>
+
+<div class="input-container">
+<form action="../../../actions/update.php" method="post">
+    <input type = "hidden" name = "id" value="<?php echo $id?>">
+    <br>
+
+    <label for="nome">Nome:</label>
+    <input type="text" name="nome" value="<?php echo $nome?>">
+
+    <br>
+
+    <label for="prezzo">Prezzo:</label>
+    <input type="text" name="prezzo" value="<?php echo $prezzo?>">
+
+    <br>
+
+    <label for="marca">Marca:</label>
+    <input type="text" name="marca" value="<?php echo $marca?>">
+
+    <br>
+
+    <input type="submit" value="Invia">
+</form>
+</div>
+
+</body>
+</html>
--- ecommerce-main/views/products/show.php
+++ ecommerce-main/views/products/show.php
@@ -2,6 +2,11 @@
 session_save_path("../../sessions");
 session_start(); // Avvia la sessione
 
+if(!isset($_SESSION['current_user']))
+{
+    header('Location: ../login.php');
+}
+
 require_once "../../database.php";
 require_once "../../models/products.php";
 
@@ -11,37 +16,50 @@ $query = "SELECT * FROM products";
 $stmt = $pdo->query($query);
 
 $products = $stmt->fetchAll(PDO::FETCH_CLASS,'models\products');
-
 ?>
-<form action="../login.php">
-    <input type="submit" value="logout"/>
-</form>
-<form action="../admin/products/create.php">
-    <input type="submit" value="Crea prodotto"/>
-</form>
 
-<hr>
-
-<div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/styles.css">
+    <title>Your Store</title>
+</head>
+<body>
+<header>
+    <form action="../logout.php">
+        <input type="submit" value="Logout"/>
+    </form>
+    <form action="../admin/products/create.php">
+        <input type="submit" value="Crea prodotto"/>
+    </form>
+</header>
+<div class="title">Gestione prodotti</div>
+<div class="products-container">
     <?php foreach ($products as $product): ?>
-        <div>
-            <label>ID:</label>
-            <div><?php echo $product->getId(); ?></div>
-        <div>
-            <label>Nome:</label>
-            <div><?php echo $product->getNome(); ?></div>
-        </div>
-        <div>
-            <label>Prezzo:</label>
-            <div><?php echo $product->getPrezzo(); ?></div>
-        </div>
-        <div>
-            <label>Marca:</label>
-            <div><?php echo $product->getMarca(); ?></div>
-        </div>
-            <form action="../admin/products/update.php">
-                <input type="submit" value="Modifica"/>
+        <div class="product">
+            <div class="product-details">
+                <label>ID:</label>
+                <div><?php echo $product->getId(); ?></div>
+                <label>Nome:</label>
+                <div><?php echo $product->getNome(); ?></div>
+                <label>Prezzo:</label>
+                <div><?php echo $product->getPrezzo(); ?></div>
+                <label>Marca:</label>
+                <div><?php echo $product->getMarca(); ?></div>
+            </div>
+            <form action="../admin/products/update.php" method="post">
+                <input type="hidden" name ="id" value="<?php  echo $product->getId();?>">
+                <input type="hidden" name ="nome" value="<?php  echo $product->getNome();?>">
+                <input type="hidden" name ="prezzo" value="<?php  echo $product->getPrezzo();?>">
+                <input type="hidden" name ="marca" value="<?php  echo $product->getMarca();?>">
+                <button type="submit" class="add-to-cart-btn">Modifica</button>
             </form>
-        <hr>
+        </div>
     <?php endforeach; ?>
 </div>
+
+<hr>
+</body>
+</html>
